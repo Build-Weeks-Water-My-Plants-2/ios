0dc6a0d2c85940a297ecc2cca554b6e004949fbe
Zach Thacker <zachthac@aol.com>
2020-06-23 22:19:31 -0400

    Set up storyboard and IBOutlets/actions

---
+++ BuildWeek2/View Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  BuildWeek2
+//
+//  Created by Clean Mac on 6/23/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+
+
+}
