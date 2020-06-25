36fce9856c47ee2c2f5a8c01b69bced9a68a9ff9
Zach Thacker <zachthac@aol.com>
2020-06-25 18:00:01 -0400

    Almost Finished Detail VC (Stuck)

--- BuildWeek2/View Controllers/DetailViewController.swift
+++ BuildWeek2/View Controllers/DetailViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
+
+
+
 class DetailViewController: UIViewController {
+    
+    var apiController: APIController?
+      var plantCell: Plant?
+    
     @IBOutlet weak var plantImageView: UIImageView!
     @IBOutlet weak var nicknameTextField: UITextField!
     @IBOutlet weak var plantSpeciesTextField: UITextField!
@@ -16,16 +23,47 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var happinessSegmentedControl: UISegmentedControl!
     @IBOutlet weak var savePlantButton: UIButton!
     
-    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getDetails()
+        
+        
         // Do any additional setup after loading the view.
     }
-    @IBAction func buttonTapped(_ sender: UIButton) {
+    
+    func getDetails() {
+        guard let apiController = apiController,
+            let plantCell = self.plantCell else { return }
+        apiController.fetchPlantsFromDatabase() { (data) in
+            if (try? data.get()) != nil {
+                DispatchQueue.main.async {
+                    self.updateViews(with: plantCell)
+                }
+            }
+        }
+        
     }
     
-
+    private func updateViews(with plant: Plant) {
+        nicknameTextField.text = plant.nickname
+        plantSpeciesTextField.text = plant.species
+        waterFrequencyTextField.text = ""
+        happinessSegmentedControl.selectedSegmentIndex = 0
+        
+        
+        
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
 
 }
+
+
+
+
+
+
+
